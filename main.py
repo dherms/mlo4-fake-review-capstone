df77c8f8bb3d9959c545829ae0fcfc36c0348729
David Griffiths Herman-Ifueko <david@davidherman.dev>
2022-11-17 08:46:16 -0800

    Add Weed.com Review endpoint

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from pydantic import BaseModel
 class Review(BaseModel):
     text: str
 
-sess = onxrt.InferenceSession("sgd_fake_review.onnx")
-input_name = sess.get_inputs()[0].name
-label_name = sess.get_outputs()[0].name
+rf_sess = onxrt.InferenceSession("rf_fake_review.onnx")
+rf_input_name = rf_sess.get_inputs()[0].name
+rf_label_name = rf_sess.get_outputs()[0].name
+
+sgd_sess = onxrt.InferenceSession("sgd_fake_review.onnx")
+sgd_input_name = sgd_sess.get_inputs()[0].name
+sgd_label_name = sgd_sess.get_outputs()[0].name
 
 app = FastAPI(title="Fake Review Detector")
 
@@ -15,7 +19,12 @@ app = FastAPI(title="Fake Review Detector")
 async def root():
     return {"message": "OK"}
 
-@app.post("/classify_review", tags=["Classify Review"])
+@app.post("/classify_review", tags=["Classify Review - General"])
 async def classify_review(review: Review):
-    pred_onx = sess.run([label_name], {input_name: [review.text]})[0]
+    pred_onx = sgd_sess.run([sgd_label_name], {sgd_input_name: [review.text]})[0]
+    return {"prediction": int(pred_onx)}
+
+@app.post("/classify_review_weed", tags=["Classify Review - Weed.com"])
+async def classify_review(review: Review):
+    pred_onx = rf_sess.run([rf_label_name], {rf_input_name: [review.text]})[0]
     return {"prediction": int(pred_onx)}
